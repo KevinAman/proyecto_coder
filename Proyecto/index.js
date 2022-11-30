77d581c405a6ab7691c49f93986724cea43e2beb
KevinAman <68305748+KevinAman@users.noreply.github.com>
2022-11-29 22:12:35 -0300

    cambios entrega 3

---
+++ Proyecto/index.js
@@ -0,0 +1,33 @@
+// alert ('Hola Bienvenido a la vinoteca')
+// let nombre = prompt('Ingrese su nombre')
+// let edad = parseInt(prompt('Ingrese su edad'))
+// if  (edad < 18) {
+//     alert('NO puede continuar navegando');
+// }
+// else {
+//     alert('Puede continuar');
+// }
+// function seguir(){
+//     alert ('Continuar');
+// }
+// const vinos = [{codigo: 1, marca: 'LasPerdices', valor: 1500},
+//                {codigo: 2, marca: 'SaintFelicien', valor: 1800},
+//                {codigo: 3, marca: 'Sapo de otro poso', valor: 2500},
+//                {codigo: 4, marca: 'DV Castena', valor: 1900}]
+// while (edad >= 18){
+//     let cantidad = parseInt(prompt('Ingrese cuantos vinos quiere comprar'));
+//     let total = 0;
+//     while (cantidad =! 0){
+        
+//         let opcion = parent(prompt('Elegir la opcion de vino para ir sumado el total'));
+//         switch (opcion) {
+//             case 1: total += 1500;
+//             case 2: total += 1800;
+//             case 3: total += 2500;
+//             case 4: total += 1900;
+//             default: alert('la opcion es incorrecta');
+//         }
+//     } 
+//     alert ('Su total es:'+ total);
+}
+
---
+++ Proyecto/js/productos.js
@@ -0,0 +1,7 @@
+const vinos = [{codigo: 1, nombre: 'vino las perdices', precio: 1290},
+               {codigo: 1, nombre: 'vino saint felicien', precio: 1390},
+               {codigo: 1, nombre: 'vino sapo de otro pozo', precio: 1500},
+               {codigo: 1, nombre: 'vino dv catena', precio: 1450}
+
+
+]
